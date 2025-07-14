d590d7e61ef2af4482234308d8c9c8a6249bfd03
Yoo <yooyoo9191@gmail.com>
2025-07-14 21:15:51 +0900

    feat. domain을 입력받고 DNS 서버 별로 응답 속도 비교 및 최소 응답 서버 출력

--- Yoo.py
+++ Yoo.py
@@ -1,34 +1,48 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-import time
+import time, json
 import dns.resolver
 
 app = FastAPI() # API 객체 생성
 
 dns_servers =  { # DNS 서버 IP 목록
     "Google" : "8.8.8.8",
-    "Cloudflare" : "1.1.1.1",
+    "KISA" : "203.248.252.2",
     "KT" : "168.126.63.1",
     "SKB" : "219.250.36.130",
     "LGU+" : "164.124.101.2",
     "Kakao" : "203.248.252.2",
-    "Quad9" : "9.9.9.9"
 }
 
-@app.get("/dns/{domain}") # domain 입력받기 > 예 : localhost.8000/dns/naver.com
+@app.get("/dns/{domain}") # domain 입력받기 > 예 : localhost.8000/dns/naver.com 네이버로
 def read_root(domain: str):
+    result_all = [] 
+    result_response = [] 
     for name, ip in dns_servers.items():
         tester = dns.resolver.Resolver()
         tester.nameservers = [ip]
-        start_time = time.time()
+        start_time =time.time()
         answer = tester.resolve(domain, 'A')
         end_time = time.time()
-        response = round((end_time - start_time) * 1000, 2)
-        
-        return{
-            "DNS 서버" : name,
-            "서버 IP" : answer,
-            "DNS" : f"{response} ms"
-        }
+        response = round((end_time - start_time) * 1000, 2) 
 
-                
+        result_all.append({
+            "DNS 서버" : name,
+            "응답 시간" : f"{response} + ms"
+        })
+        result_response.append(response)
+    
+    min_server_name = None
+    min_response = min(result_response)
+    for item in result_all:
+        if float(item["응답 시간"].replace(" + ms", "")) == min_response:
+            min_server_name = item["DNS 서버"]
+    
+    result_final = {
+        "도메인" : domain,
+        "전체 결과" : result_all,
+        "최소 응답 서버" : min_server_name,
+        "응답 시간" : f"{min_response} ms"
+    }
+
+    return result_final

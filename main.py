93e156fb0fe22659ca6d3a4bad5dd302f239b5ce
hoongi7 <gnsrl1799@gmail.com>
2025-07-24 14:44:26 +0900

    옵션위젯 공부

--- main.py
+++ main.py
@@ -1,57 +1,67 @@
 from tkinter import *
-import random
-from datetime import datetime
+from tkinter.ttk import *
+
 win = Tk()
-win.title("AIM_GAME")
-win.geometry("550x150")
+win.geometry("500x500+950+0")
+win.option_add("*Font", "Arial 20")
+
+# ListBox
+# lb = Listbox(win)
+# lb.config(selectmode="multiple") # 다중선택 옵션
+# lb.insert(0, "1번")
+# lb.insert(1, "2번")
+# lb.insert(2, "3번")
+# lb.insert(3, "4번")
+# lb.pack()
+
+# # CheckButton
+# cv = IntVar()
+# cb = Checkbutton(win, text = "1번", variable=cv)
+# cb.pack()
+
+# # RadioButton
+# rv = IntVar()
+# rb1 = Radiobutton(win, text = "1번", value = 0, variable = rv)
+# rb2 = Radiobutton(win, text = "2번", value = 1, variable = rv)
+# rb3 = Radiobutton(win, text = "3번", value = 3, variable = rv)
+# rb1.pack()
+# rb2.pack()
+# rb3.pack()
+
+# # Combobox
+# cb_list = ["1", "2", "3"]
+# cb = Combobox(win)
+# cb.config(values = cb_list)
+# cb.pack()
+
+# # SpinBox
+# sb = Spinbox(win)
+# sb.config(from_=-1, to=1)
+# sb.pack()
+
+# Scale
+scale = Scale(win)
+scale = Scale(win, length=300, from_=0, to=50, orient="horizontal")
+scale.pack()
+
+
+# BUTTON
+btn = Button(win)
+btn.config(text = "옵션 선택")
+def click():
+    # text = lb.curselection()[0] #ListBox
+    # text = cv.get() #CheckButton
+    # lab_text = rv.get() # RadioButton
+    # lab_text = cb.get() # ComboBox
+    # lab_text = sb.get() # SpinBox
+    lab_text = scale.get()
+    lab.config(text = lab_text)
+btn.config(command = click)
+btn.pack()
 
 # Label
 lab = Label(win)
-lab.config(text = "표적 개수")
-lab.grid(column=0, row= 0, padx=20, pady=20)
+lab.pack()
 
-# Entry
-ent = Entry(win)
-ent.grid(column=1, row=0, padx=20, pady=20)
-
-k = 1
-
-def cc():
-    global k
-    if k < num_t:
-        k += 1
-        btn.destroy()
-        ran_btn()
-    else:
-        fin = datetime.now()
-        dif_sec = (fin-start).total_seconds()
-        btn.destroy()
-        lab = Label(win)
-        lab.config(text = "Clear " + str(dif_sec) + "초")
-        lab.pack(pady = 230)
-
-def ran_btn():
-    global btn
-    btn = Button(win)
-    btn.config(bg = "red")
-    btn.config(command = cc)
-    btn.config(text = k)
-    btn.place(relx= random.random(), rely = random.random())
-
-def btn_f():
-    global num_t
-    global start
-    num_t = int(ent.get())
-    for wg in win.grid_slaves(): # [label, entry, button]
-        wg.destroy()
-    win.geometry("500x500")
-    ran_btn()
-    start = datetime.now()
-
-# Button
-btn = Button(win)
-btn.config(text = "시작")
-btn.config(command = btn_f)
-btn.grid(column=0, row=1, columnspan=2)
 
 win.mainloop()
